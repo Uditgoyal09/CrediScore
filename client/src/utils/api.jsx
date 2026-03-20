5ad82925a6fd53665755450b6606bb388b3950df
Udit Goyal <uditgoyal0905@example.com>
2026-03-20 23:03:52 +0530

    adding Vercel Json

--- client/src/utils/api.jsx
+++ client/src/utils/api.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
-const apiBaseUrl = import.meta.env.VITE_API_BASE_URL?.trim() || "/api";
+if (!BASE_URL) {
+    throw new Error("VITE_API_URL is not defined");
+}
 
 const api = axios.create({
-    baseURL: apiBaseUrl,
-});
+    baseURL: BASE_URL + "/api",
+    withCredentials: true
+})
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
